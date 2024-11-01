0155573774b02de5b163a3898cf7f243ae918ebe
Max Teplov <153938230+43inviz@users.noreply.github.com>
2024-11-01 18:14:41 +0100

    hw_smtp

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Mail;
+
+namespace HW_smtp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            List<string> recipients = new List<string>();
+
+            MailAddress from = new MailAddress("ex@mail.com", "Tom");
+            MailMessage mess = new MailMessage()
+            {
+                Subject = "Text",
+                Body = "Mail test",
+                IsBodyHtml = true,
+                From = from
+            };
+
+            foreach (string recipient in recipients)
+            {
+                mess.To.Add(recipient);
+            }
+             
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com",587);
+            smtp.Credentials = new NetworkCredential("somemail@gmail.com", "Application password");
+            smtp.EnableSsl = true;
+            smtp.Send(mess);
+        }
+    }
+}
